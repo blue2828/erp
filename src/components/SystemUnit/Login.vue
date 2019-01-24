2bb9002e165bb11e2a6c86c267eb5e8e10d0fa4e
blue2828 <651991020@qq.com>
2019-01-24 16:03:52 +0800

    test

--- src/components/SystemUnit/Login.vue
+++ src/components/SystemUnit/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <form>
-    <input placeholder="输入账号" name="account" v-model="">
-    <input placeholder="输入密码" name="pwd" v-model="">
+    <input placeholder="输入账号" name="account" v-model=""><br>
+    <input placeholder="输入密码" name="pwd" v-model=""><br>
   </form>
 </template>
 
